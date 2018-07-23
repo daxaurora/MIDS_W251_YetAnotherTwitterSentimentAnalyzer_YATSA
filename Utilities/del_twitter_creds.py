f9b5782610cd3b056bed9a3948184f93fdd09e19
root <root@gateway.w251.mids>
2018-07-23 11:33:51 -0500

    added comment into delete about using lowercase

--- Utilities/del_twitter_creds.py
+++ Utilities/del_twitter_creds.py
@@ -7,5 +7,5 @@ if len(sys.argv) != 2:
 
 cluster=Cluster(['cassandra1','cassandra2','cassandra3'])
 session=cluster.connect('w251twitter')
+# FOR SOME REASON THE DELETE MUST BE IN LOWERCASE
 session.execute("delete from twitter_connections where username = %s",(sys.argv[1],))
-
