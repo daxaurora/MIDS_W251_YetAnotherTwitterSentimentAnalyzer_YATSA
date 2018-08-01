bfcfdbeab9a4bac144bbb7f799ece4d562999cbd
Walt Burge <walt.burge@berkeley.edu>
2018-08-01 12:01:13 -0500

    base end-to-end functionality, including summarized sentiment scores

--- streaming/spark_pull_sentiments.py
+++ streaming/spark_pull_sentiments.py
@@ -13,29 +13,41 @@ def derive_sentiment(sent, sentValue):
         return sentValue
 
 
-def removeNonAscii(s): return "".join(filter(lambda x: ord(x)<128, s))
+def remove_non_ascii(s): return "".join(filter(lambda x: ord(x)<128, s))
 
 
 def get_sentiment(tweet_text):
     nlp = StanfordCoreNLP('http://localhost:9000')
     sentiment = 0
     if tweet_text != None:
-        response = nlp.annotate(removeNonAscii(tweet_text).encode('ascii'),
+	print("Text to corenlop:\n{}".format(tweet_text))
+
+        response = nlp.annotate(tweet_text.encode('ascii'),
                                 properties={
                                     'annotators': 'sentiment',
                                     'outputFormat': 'json',
                                     'timeout': 1000,
                                 })
 
-	print("Sentiment response:\n{}".format(response))
+	#print("Sentiment response:\n{}".format(response))
 
-        for s in response['sentences']:
-	    sent_response = json.loads(s)
-            ind = sent_response["index"]
-            words = " ".join([t["word"] for t in sent_response["tokens"]])
-            sent = sent_response["sentiment"]
-            sent_value = sent_response["sentimentValue"]
-            sentiment += derive_sentiment(sent, sent_value)
+        if isinstance(response, dict):
+	    for s in response['sentences']:
+	        #print("Sentence: \"{}\"i\n".format(s))
+                ind = s["index"]
+                print("ind: {}".format(ind))
+	        words = " ".join([t["word"] for t in s["tokens"]])
+                print("words: {}".format(words))
+	        if "hashtags" in s:
+                    hashtags = " ".join([t["hashtags"] for t in s["hashtags"]])
+	            print("hashtags: {}".format(hashtags))
+	        else:
+		    print("(no hashtags)")
+                sent = s["sentiment"]
+                print("sent: {}".format(sent))
+                sent_value = s["sentimentValue"]
+                print("sent_value: {}".format(sent_value))
+                sentiment += derive_sentiment(sent, int(sent_value))
 	
 	print("Returning derived sentiment: {}".format(sentiment))
 
@@ -49,7 +61,7 @@ if __name__ == "__main__":
     kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
     tweets = kvs.map(lambda x: json.loads(x[1]))
     tweets.count().map(lambda x: 'Tweets in this batch: %s' % x).pprint()
-    text_dstream = tweets.map(lambda tweet: json.loads(tweet)['text'])
+    text_dstream = tweets.map(lambda tweet: remove_non_ascii(json.loads(tweet)['text']))
 
     sentiments = text_dstream.map(lambda tweet_text: (get_sentiment(tweet_text), tweet_text))
     sentiments.pprint()
