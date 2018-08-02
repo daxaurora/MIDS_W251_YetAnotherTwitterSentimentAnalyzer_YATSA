e2024a0de77051b2508766a962b94bb411b5df32
Walt Burge <walt.burge@berkeley.edu>
2018-08-01 21:25:16 -0500

    added saving tweets to Cassandra

--- streaming/twitter_connect_json.py
+++ streaming/twitter_connect_json.py
@@ -1,6 +1,5 @@
 import sys
 from cassandra.cluster import Cluster
-
 import dateutil.parser
 import json
 from pyspark import SparkContext
@@ -17,26 +16,30 @@ access_token_secret = "yaG9Qpjm5fmSyE2L5hU8uM54pvLbY5eC0iAzWms9Xd2CB"
 consumer_key = "lMHMMbmNxtlLHcZA9Bqxh8h6w"
 consumer_secret = "KAT8bFoZknI1TYnx19Gx6p4cQqMbTtFAUOoBf1ndmHQ7eskgEX"
 
-SAMPLE_TWEET = '{"created_at": "Tue Jul 31 22:11:14 +0000 2018", "id": 1024417228400119809, "id_str": "1024417228400119809", "text": "RT @Astro_WRLD: Like to join an e-dating gc", "source": "\u003ca href=\"http:\/\/twitter.com\/download\/iphone\" rel=\"nofollow\"\u003eTwitter for iPhone\u003c\/a\u003e", "truncated": false, "in_reply_to_status_id": null, "in_reply_to_status_id_str": null, "in_reply_to_user_id": null, "in_reply_to_user_id_str": null, "in_reply_to_screen_name": null, "user": {"id": 787053226264698880, "id_str": "787053226264698880", "name": "\ud835\udd41\ud835\udd60\ud835\udd60\ud83d\ude80\ud83c\udf0f", "screen_name": "Astro_WRLD", "location": "Los Angeles, CA", "url": null, "description": "@Zendaya @Lakers @Dodgers @KingJames @B_Ingram13 @KendrickLamer @TrvisXX", "translator_type": "none", "protected": false, "verified": false, "followers_count": 1031, "friends_count": 847, "listed_count": 6, "favourites_count": 3467, "statuses_count": 5022, "created_at": "Fri Oct 14 22:11:26 +0000 2016", "utc_offset": null, "time_zone": null, "geo_enabled": false, "lang": "en", "contributors_enabled": false, "is_translator": false, "profile_background_color": "F5F8FA", "profile_background_image_url": "", "profile_background_image_url_https": "", "profile_background_tile": false, "profile_link_color": "1DA1F2", "profile_sidebar_border_color": "C0DEED", "profile_sidebar_fill_color": "DDEEF6", "profile_text_color": "333333", "profile_use_background_image": true, "profile_image_url": "http:\/\/pbs.twimg.com\/profile_images\/1024396180539682816\/Yf7iXooA_normal.jpg", "profile_image_url_https": "https:\/\/pbs.twimg.com\/profile_images\/1024396180539682816\/Yf7iXooA_normal.jpg", "profile_banner_url": "https:\/\/pbs.twimg.com\/profile_banners\/787053226264698880\/1533072450", "default_profile": true, "default_profile_image": false, "following": null, "follow_request_sent": null, "notifications": null}, "geo": null, "coordinates": null, "place": null, "contributors": null, "retweeted_status": {"created_at": "Tue Jul 31 08:02:38 +0000 2018", "id": 1024203669854539777, "id_str": "1024203669854539777", "text": "Like to join an e-dating gc", "source": "\u003ca href=\"http:\/\/twitter.com\/download\/iphone\" rel=\"nofollow\"\u003eTwitter for iPhone\u003c\/a\u003e", "truncated": false, "in_reply_to_status_id": null, "in_reply_to_status_id_str": null, "in_reply_to_user_id": null, "in_reply_to_user_id_str": null, "in_reply_to_screen_name": null, "user": {"id": 787053226264698880, "id_str": "787053226264698880", "name": "\ud835\udd41\ud835\udd60\ud835\udd60\ud83d\ude80\ud83c\udf0f", "screen_name": "Astro_WRLD", "location": "Los Angeles, CA", "url": null, "description": "@Zendaya @Lakers @Dodgers @KingJames @B_Ingram13 @KendrickLamer @TrvisXX", "translator_type": "none", "protected": false, "verified": false, "followers_count": 1031, "friends_count": 847, "listed_count": 6, "favourites_count": 3467, "statuses_count": 5021, "created_at": "Fri Oct 14 22:11:26 +0000 2016", "utc_offset": null, "time_zone": null, "geo_enabled": false, "lang": "en", "contributors_enabled": false, "is_translator": false, "profile_background_color": "F5F8FA", "profile_background_image_url": "", "profile_background_image_url_https": "", "profile_background_tile": false, "profile_link_color": "1DA1F2", "profile_sidebar_border_color": "C0DEED", "profile_sidebar_fill_color": "DDEEF6", "profile_text_color": "333333", "profile_use_background_image": true, "profile_image_url": "http:\/\/pbs.twimg.com\/profile_images\/1024396180539682816\/Yf7iXooA_normal.jpg", "profile_image_url_https": "https:\/\/pbs.twimg.com\/profile_images\/1024396180539682816\/Yf7iXooA_normal.jpg", "profile_banner_url": "https:\/\/pbs.twimg.com\/profile_banners\/787053226264698880\/1533072450", "default_profile": true, "default_profile_image": false, "following": null, "follow_request_sent": null, "notifications": null}, "geo": null, "coordinates": null, "place": null, "contributors": null, "is_quote_status": false, "quote_count": 0, "reply_count": 10, "retweet_count": 2, "favorite_count": 32, "entities": {"hashtags": [], "urls": [], "user_mentions": [], "symbols": []}, "favorited": false, "retweeted": false, "filter_level": "low", "lang": "en"}, "is_quote_status": false, "quote_count": 0, "reply_count": 0, "retweet_count": 0, "favorite_count": 0, "entities": {"hashtags": [], "urls": [], "user_mentions": [{"screen_name": "Astro_WRLD", "name": "\ud835\udd41\ud835\udd60\ud835\udd60\ud83d\ude80\ud83c\udf0f", "id": 787053226264698880, "id_str": "787053226264698880", "indices": [3, 14]}], "symbols": []}, "favorited": false, "retweeted": false, "filter_level": "low", "lang": "en", "timestamp_ms": "1533075074659"}'
+
 class StdOutListener(StreamListener):
+
+
+    def __init__(self):
+        cluster=Cluster(['cassandra1','cassandra2','cassandra3'])
+        session=cluster.connect('w251twitter')
+
     def on_data(self, data):
         if data and ('delete' not in data):
-	    save_tweet(data)
+            session.execute(
+                    """
+                    INSERT INTO TWEETS(tweet, insertion_time) VALUES (data.encode('utf-8'), toTimestamp(now())
+                    """)
+
             tweet_json = get_tweet_json(data)
             if tweet_json:
-		session.("INSERT INTO TWEETS(tweet, insertion_time) VALUES (data.encode('utf-8'), toTimestamp(now())
                 producer.send_messages("twitter", tweet_json.encode('utf-8'))
                 print(tweet_json)
         return True
 
+
     def on_error(self, status):
         print(status)
-
-    def save_tweet(data):
-	session.execute(
-		"""
-		INSERT INTO TWEETS (tweet, insertion_time) VALUES (data, toTimestamp(now())
-		""")
 
 
 class Tweet(dict):
