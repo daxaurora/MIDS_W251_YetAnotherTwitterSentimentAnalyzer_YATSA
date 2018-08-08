789b8af257c1820f929068f562421f825b289fba
daxaurora <daxaurora@gmail.com>
2018-08-08 12:00:09 -0700

    Test basic streaming process

--- streaming/spark_process.py
+++ streaming/spark_process.py
@@ -1,60 +1,88 @@
 import sys
+import json
+import datetime
+import time
+import dateutil.parser
+from datetime import timedelta
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-import json
+from pycorenlp import StanfordCoreNLP
+from pyspark_cassandra import streaming
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
 
-window_size = 30
+# Define batch size in seconds
+window_size = 15
 
-def preprocess(tweet):
-    # Manage errors if an empty tweet sneaks through the pipeline
+def get_concept():
+    """ This function will query the Cassandra database to match hashtags
+    in a tweet with a corresponding concept.
+    """
+    concept = "Ant Man and The Wasp"
+    return concept
+
+def get_tweet_text(tweet):
+    """This function takes the full content of a tweet and extracts the text
+    of the tweet with ascii encoding.
+    """
+    # Manage errors for empty tweets
     if tweet.get('text')==None:
         tweet_text = None
     else:
         tweet_text = tweet['text'].encode('ascii','ignore').split(" ")
     return tweet_text
 
-def sentiment_analysis():
+def get_sentiment(tweet_text):
+    """This function takes in the text in a tweet and returns a sentiment
+    value using Stanford CoreNLP: https://stanfordnlp.github.io/CoreNLP/
+    """
+    sentiment = 3
+    return sentiment
+
+def summarize_tweets(tweet):
+    """This function takes in each tweet and returns a tuple with 4 elements
+    for entering into the "summary" Cassandra database:
+    concept (primary key), insertion_time, sentiment, tweet_count
+    """
     return None
 
-
 def main():
-    sc = SparkContext(appName="ParseDataFromTwitterStream")
-    # Above here is different in Walt's
+    # Set up Spark session and connection with Cassandra
+    sparkConf = SparkConf().setAppName("TwitterSentimentAnalysisByHashtag") \
+        .set("spark.cassandra.connection.host", "cassandra1, cassandra2, cassandra3")
+    sc = SparkContext(conf=sparkConf)
+    session = SparkSession(sc)
+    sqlContext = SQLContext(sc)
     ssc = StreamingContext(sc, window_size)
+    # Set up receiving Twitter stream from Kafka
     brokers, topic = sys.argv[1:]
     kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
-    # Below here is different in Walt's
-    tweets = kvs.map(lambda x: json.loads(x[1]))
-    tweets.count().map(lambda x:'Total number of tweets related to topic in this %d-second batch: %s'
+    # Set up connection to Stanford NLP Sentiment analysis
+    nlp = StanfordCoreNLP('http://localhost:9000')
+    # Parse tweets into RDD
+    tweets = kvs.filter(lambda x: x is not None).filter(lambda x: x is not '').map(lambda x: json.loads(x[1]))
+
+    # print statements for testing - remove or comment out for final production
+    tweets.count().map(lambda x:'Total number of tweets in this %d-second batch: %s'
                                 % (window_size, x)).pprint()
-    tweets.map(lambda tweet: preprocess(tweet)).pprint()
+    tweets.map(lambda tweet: get_tweet_text(tweet)).pprint()
+
+    # Create RDD to summarize tweets for each window for summary DB table
+    # summary_stream = tweets.map(lambda tweet: summarize_tweets(tweet)).filter(lambda x: x is not None)
+    # This stream will also need to aggregate/reduce by key for concept
+    # summary_stream.saveToCassandra("w251twitter", "summary")
+    # Create RDD to extract tweet data for sentiment DB table
+    # Here's where Walt's code for the sentiment_stream will go
+    # Need other functions in place to make the sentiment_stream work
+    
+    # Start Spark
     ssc.start()
     ssc.awaitTermination()
 
+
+
 if __name__ == "__main__":
     main()
-
-# From Walt's code, for comparison:
-if __name__ == "__main__":
-        sparkConf = SparkConf().setAppName("TwitterSentimentAnalysis") \
-            .set("spark.cassandra.connection.host", "cassandra1, cassandra2, cassandra3")
-
-        sc = SparkContext(conf=sparkConf)
-        session = SparkSession(sc)
-        sqlContext = SQLContext(sc)
-        ssc = StreamingContext(sc, 2)
-        brokers, topic = sys.argv[1:]
-
-        kvs = setup_kafka_stream()
-
-        nlp = StanfordCoreNLP('http://localhost:9000')
-
-        tweets = kvs.filter(lambda x: x is not None).filter(lambda x: x is not '').map(lambda x: json.loads(x[1]))
-        tweets.count().map(lambda x: 'Tweets in this batch: %s' % x).pprint()
-
-        sentiment_stream = tweets.map(lambda tweet: get_tweet_sentiment(tweet))
-        sentiment_stream.foreachRDD(lambda rdd : save_tweet_to_cassandra(rdd))
-
-        ssc.start()
-        ssc.awaitTermination()
