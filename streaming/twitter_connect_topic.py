6cca7c126500284b8d82d185abba482219da1954
daxaurora <daxaurora@gmail.com>
2018-08-08 11:24:29 -0700

    Test new Twitter Stream

--- streaming/twitter_connect_topic.py
+++ streaming/twitter_connect_topic.py
@@ -2,13 +2,23 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from kafka import SimpleProducer, KafkaClient
+from cassandra.cluster import Cluster
 
-# Twitter credentials
-# Could configure this to get them from Cassandra
-access_token="798563487886606336-VmKXDY8NYOnoCf6TXBA7jjnXWPFXlM2"
-access_token_secret="cgsRhCZsipYTQuwbSWgRrcW76VEyrYOfGbUHMQBVvMq4j"
-consumer_key="ZSBVncc7Ducoi1wpagg1cVwZx"
-consumer_secret="iHzPeZe6mY5YhwK43EsQwnXHMfAEzvcAJ1nsR8LCGSpeeflCgk"
+# Connect to Cassandra
+cluster=Cluster(['cassandra1','cassandra2','cassandra3'])
+session=cluster.connect('w251twitter')
+# Get hashtags from Cassandra
+tags = session.execute('select * from concepts')
+hashtags = []
+for tag in tags:
+    hashtags.extend(row.hashtag_list)
+# Get Twitter credentials from Cassandra
+creds = session.execute("select * from twitter_connections where username = 'laura'")
+for cred in creds:
+    access_token = cred.access_token
+    access_token_secret = cred.access_token_secret
+    consumer_key = cred.consumer_key
+    consumer_secret = cred.consumer_secret
 
 # Define stream
 class StdOutListener(StreamListener):
@@ -16,30 +26,24 @@ class StdOutListener(StreamListener):
     def on_data(self,data):
         if data and ('delete' not in data):
             producer.send_messages("twitter", data.encode('utf-8'))
+            print(tweet_json)
         return True
 
     def on_error(self, status):
-        # If the Twitter account used to produce tweets exceeds rate limit,
+        # If the Twitter account used to produce tweets exceeds the rate limit,
         # continued failed attempts to connect will exponentially increase
         # the time until the next successful connection.
-        # This code will cut off the stream if it's maxed out and prevents
-        # repeated attempts from delaying reconnection.
-        # This solution is probably not needed for enterprise solutions
-        # that don't have rate limiting.
+        # This code prevents repeated attempts from delaying reconnection.
+        # Not required for an enterprise solution with unlimited streaming
         if status_code == 420:
             return False
 
-# Set up Kafka producer from Twitter 
+# Set up Kafka producer from Twitter
 kafka = KafkaClient("localhost:9092")
 producer=SimpleProducer(kafka)
 l = StdOutListener()
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 stream = Stream(auth, l)
-# Filter for tracking hashtags is case sensitive
 stream.filter(languages = ['en'],
-              track = [
-                 '#antman', '#ant-man', '#antmanandthewasp',
-                 '#AntMan', '#Ant-Man', '#Ant-man', 'Antman',
-                 'TheWasp', '#AntManandTheWasp', '#AntManAndTheWasp',
-                  'trump', 'Trump'])
+              track = hashtags)
